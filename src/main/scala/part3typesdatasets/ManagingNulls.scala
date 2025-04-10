f9ab4361a50b87f7cdeb71d5fe21292049dd5e59
nlscng <nlscng.g@gmail.com>
2025-04-10 05:50:48 -0500

    Managing nulls, first none null, null last, remove, and default.

--- src/main/scala/part3typesdatasets/ManagingNulls.scala
+++ src/main/scala/part3typesdatasets/ManagingNulls.scala
@@ -1,6 +1,7 @@
 package part3typesdatasets
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{coalesce, col}
 
 object ManagingNulls extends App {
   val spark = SparkSession.builder()
@@ -12,4 +13,32 @@ object ManagingNulls extends App {
     .option("inferSchema", "true")
     .json("src/main/resources/data/movies.json")
 
+  // select the first non-null value
+  val moviesWithFirstNotNullRating = moviesDF.select(
+    col("Title"),
+    coalesce(col("Rotten_Tomatoes_Rating"), col("IMDB_Rating") * 10), // coalesce is firstNonNull
+    col("Rotten_Tomatoes_Rating"),
+    col("IMDB_Rating")
+  )
+
+  println(s"Movies with first none null ratings:")
+  moviesWithFirstNotNullRating.show()
+
+  // checking for nulls
+  moviesDF.select("*").where(col("Rotten_Tomatoes_Rating").isNull)
+
+  // nulls when ordering
+  moviesDF.orderBy(col("IMDB_Rating").desc_nulls_last)
+
+  // removing nulls
+  moviesDF.select(
+    "Title",
+    "IMDB_Rating"
+  ).na.drop() // removes rows containing nulls. na is a special object
+
+  // replace nulls
+  val moviesWithRatingNullDefault = moviesDF.na.fill(0, List("IMDB_Rating", "Rotten_Tomatoes_Rating")) // 0 for the two columns where null shows up
+
+  println(s"Movies with null IMDB or RT rating replaced with 0:")
+  moviesWithRatingNullDefault.show()
 }
