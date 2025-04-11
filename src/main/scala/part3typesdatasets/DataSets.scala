0814d36c5112e8edddef25a3d551235c9f9b2ca7
nlscng <nlscng.g@gmail.com>
2025-04-11 05:43:41 -0500

    Datasets, init and adding a csv for later practices.

---
+++ src/main/scala/part3typesdatasets/DataSets.scala
@@ -0,0 +1,18 @@
+package part3typesdatasets
+
+import org.apache.spark.sql.SparkSession
+
+object DataSets extends App {
+  val spark = SparkSession.builder()
+    .appName("Datasets")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val numbersDF = spark.read
+    .format("csv")
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .load("src/main/resources/data/numbers.csv")
+
+  numbersDF.printSchema()
+}
